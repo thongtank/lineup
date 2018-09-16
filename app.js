cb120a489d7d73315e9bcd10f08403191420aed9
panchai prasompech <panchaiprasompech@panchais-MacBook-Pro.local>
2018-09-16 17:15:03 +0700

    done for add logo but not for formation

--- app.js
+++ app.js
@@ -2,97 +2,75 @@ const $div = document.querySelector('div#container');
 var width = $div.offsetWidth;
 var height = $div.offsetHeight;
 
-var tween = null;
+$(function () {
+    // get all leagues
+    const $s_league = $('select#league');
+    const $s_team = $('select#team');
+    const $s_formation = $('select#formation');
 
-function addStar(layer, stage) {
-    var scale = Math.random();
-
-    var star = new Konva.Star({
-        x: Math.random() * stage.getWidth(),
-        y: Math.random() * stage.getHeight(),
-        numPoints: 5,
-        innerRadius: 30,
-        outerRadius: 50,
-        fill: '#89b717',
-        opacity: 0.8,
-        draggable: true,
-        scale: {
-            x: scale,
-            y: scale
-        },
-        rotation: Math.random() * 180,
-        shadowColor: 'black',
-        shadowBlur: 10,
-        shadowOffset: {
-            x: 5,
-            y: 5
-        },
-        shadowOpacity: 0.6,
-        startScale: scale
+    $.getJSON('./data/league.json', function (data) {
+        $.each(data, function (key, val) {
+            $s_league.append('<option value="' + key + '">' + val.name + '</option>');
+        });
     });
 
-    layer.add(star);
-}
-var stage = new Konva.Stage({
-    container: 'container',
-    width: width,
-    height: height
-});
+    $.getJSON('./data/formation.json', function (data) {
+        $.each(data, function (key, val) {
+            $s_formation.append('<option value="' + key + '">' + val.plan + '</option>');
+        });
+    });
 
-var layer = new Konva.Layer();
-var dragLayer = new Konva.Layer();
-
-for (var n = 0; n < 10; n++) {
-    addStar(layer, stage);
-}
-
-stage.add(layer);
-stage.add(dragLayer);
-
-// bind stage handlers
-stage.on('mousedown', function (evt) {
-    var shape = evt.target;
-    shape.moveTo(dragLayer);
-    stage.draw();
-    // restart drag and drop in the new layer
-    shape.startDrag();
-});
-
-stage.on('mouseup', function (evt) {
-    var shape = evt.target;
-    shape.moveTo(layer);
-    stage.draw();
-});
-
-stage.on('dragstart', function (evt) {
-    var shape = evt.target;
-    if (tween) {
-        tween.pause();
-    }
-    shape.setAttrs({
-        shadowOffset: {
-            x: 15,
-            y: 15
-        },
-        scale: {
-            x: shape.getAttr('startScale') * 1.2,
-            y: shape.getAttr('startScale') * 1.2
+    $s_league.change(function () {
+        let league = $(this).val();
+        $s_team.html('<option value="">Select Team</option>');
+        if (league != '') {
+            $.getJSON('./data/league.json', function (data) {
+                if (data[league].hasOwnProperty('teams')) {
+                    $.each(data[league].teams, function (key, val) {
+                        // $s_team.append('<option value="' + val + '">' + val.charAt(0).toUpperCase() + val.substr(1) + '</option>');
+                        $s_team.append('<option value="./img/logo/' + league + '/' + val.logo + '">' + val.name + '</option>');
+                    });
+                }
+            });
         }
     });
-});
 
-stage.on('dragend', function (evt) {
-    var shape = evt.target;
+    $('button#create').click(function () {
+        let img_src = $s_team.val();
+        let formation = $s_formation.val();
 
-    tween = new Konva.Tween({
-        node: shape,
-        duration: 0.5,
-        easing: Konva.Easings.ElasticEaseOut,
-        scaleX: shape.getAttr('startScale'),
-        scaleY: shape.getAttr('startScale'),
-        shadowOffsetX: 5,
-        shadowOffsetY: 5
+        var stage = new Konva.Stage({
+            container: 'container',
+            width: width,
+            height: height
+        });
+        var layer = new Konva.Layer();
+        stage.add(layer);
+
+        for (let i = 0; i < 11; i++) {
+            let imageObj = new Image();
+            imageObj.src = img_src;
+            imageObj.onload = function () {
+                var darthVaderImg = new Konva.Image({
+                    image: imageObj,
+                    x: 317,
+                    y: 100,
+                    width: 80,
+                    height: 80,
+                    draggable: true
+                });
+
+                // add cursor styling
+                darthVaderImg.on('mouseover', function () {
+                    document.body.style.cursor = 'pointer';
+                });
+                darthVaderImg.on('mouseout', function () {
+                    document.body.style.cursor = 'default';
+                });
+
+                layer.add(darthVaderImg);
+                stage.add(layer);
+            };
+        }
     });
-
-    tween.play();
 });
