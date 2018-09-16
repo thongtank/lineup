b2eff5d79c999393741025826fcbcb88415af87c
panchai prasompech <panchaiprasompech@panchais-MacBook-Pro.local>
2018-09-17 00:34:25 +0700

    D3M0 1st time

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ $(function () {
         });
     });
 
+    var formationJSON;
+    $.getJSON('./data/formation.json', function (data) {
+        formationJSON = data;
+    });
+
+    // Fill Team after choose league
     $s_league.change(function () {
         let league = $(this).val();
         $s_team.html('<option value="">Select Team</option>');
@@ -39,14 +45,64 @@ $(function () {
         let img_src = $s_team.val();
         let formation = $s_formation.val();
 
+        // Begin - create stage for field
         var stage = new Konva.Stage({
             container: 'container',
             width: width,
             height: height
         });
+
         var layer = new Konva.Layer();
         stage.add(layer);
 
+        var bgObj = new Image();
+        bgObj.onload = function () {
+            var bg = new Konva.Image({
+                image: bgObj,
+                x: 0,
+                y: 0,
+                draggable: false
+            });
+
+            // add the shape to the layer
+            layer.add(bg);
+
+            // add the layer to the stage
+            stage.add(layer);
+        };
+        bgObj.src = './img/Field.jpg';
+        // End - create stage fot field
+
+        $.each(formationJSON[formation], function (key, val) {
+            // console.log(val);
+            if (key == 'plan') {
+                return true;
+            }
+            let imageObj = new Image();
+            imageObj.src = img_src;
+            imageObj.onload = function () {
+                var logoTeam = new Konva.Image({
+                    image: imageObj,
+                    x: val.x - 21,
+                    y: val.y - 21,
+                    width: 70,
+                    height: 70,
+                    draggable: true
+                });
+
+                // add cursor styling
+                logoTeam.on('mouseover', function () {
+                    document.body.style.cursor = 'pointer';
+                });
+                logoTeam.on('mouseout', function () {
+                    document.body.style.cursor = 'default';
+                });
+
+                layer.add(logoTeam);
+                stage.add(layer);
+            };
+        });
+        /*
         for (let i = 0; i < 11; i++) {
             let imageObj = new Image();
             imageObj.src = img_src;
@@ -72,5 +128,6 @@ $(function () {
                 stage.add(layer);
             };
         }
+        */
     });
 });
